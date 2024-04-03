e306904561211452455007fc5fdad13b8681cc3b
Yin <doyin@deloitte.com.cn>
2024-04-03 14:16:23 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,8 @@
+const uniqueArray = require('./uniqueArray')
+const capitalizeFirstLetter = require('./capitalizeFirstLetter')
+const isOdd = require('./isOdd')
+const arrayAverage = require('./arrayAverage')
+
+module.exports = (arr) => {
+    return arr.join("");
+  }
